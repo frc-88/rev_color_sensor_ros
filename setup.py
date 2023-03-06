bb902ce9685e252eaf23f47472cbc1f18e84c62b
Woz4tetra <woz4tetra@gmail.com>
2023-03-06 15:51:01 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['rev_color_sensor_ros'],
-    package_dir={'': 'src'},
+    package_dir=find_packages('src'),
     )
 
 setup(**setup_args)
